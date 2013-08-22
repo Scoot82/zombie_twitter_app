aaeb275cdaa668145751017929a102f973c29bef
Scott Fey <scottfey@gmail.com>
2013-08-22 11:16:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZombieTwitterApp::Application.config.secret_key_base = 'cf8ea39c4a1ea83971692c58dfa8dea0a33746d3ebffc1d70b363396bea06e9a81b38b0d970c999d23e6057c2301e551dda5957aa62b643a2c67e882912a1781'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZombieTwitterApp::Application.config.session_store :cookie_store, key: '_zombie_twitter_app_session'
