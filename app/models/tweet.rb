3743e6c956cd41ccc92f656bb26edf8c3e1b7e04
Scott Fey <scottfey@gmail.com>
2013-08-23 13:39:04 -0500

    Add images to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ActiveRecord::Base
   scope :recent, -> { order('created_at desc').limit(10) }
-  attr_accessible :status, :location, :user_id
+  attr_accessible :status, :location, :user_id, :image
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130822170335) do
+ActiveRecord::Schema.define(version: 20130823174221) do
 
   create_table "tweets", force: true do |t|
     t.string   "status"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130822170335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
