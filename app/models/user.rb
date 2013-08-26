9fc1ec855cb51495e64072e4ff6ed0e923f61fbf
Scott Fey <scottfey@gmail.com>
2013-08-26 10:08:03 -0500

    Add image to user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :login, :password, :password_confirmation, :email, :location, :death
+  attr_accessible :login, :password, :password_confirmation, :email, :location, :death, :image
   has_secure_password
   has_many :tweets, dependent: :destroy
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130823174221) do
+ActiveRecord::Schema.define(version: 20130826145949) do
 
   create_table "tweets", force: true do |t|
     t.string   "status"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20130823174221) do
     t.date     "death"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
